c6d965c84f931c943d80f60545151cc447e1fb23
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-02-11 15:39:24 +0530

    add assets and pages

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const MyProfile = () => {
   return (
     <div>
         
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default MyProfile
