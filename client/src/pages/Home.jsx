d2b2e830a780b3757205f367b173560fe15b3451
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-02-12 22:48:54 +0530

    add Top Doctors section in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecalityMenu from '../components/SpecalityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
         <Header/>
         <SpecalityMenu/>
+        <TopDoctors/>
     </div>
   )
 }
