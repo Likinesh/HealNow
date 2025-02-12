982597d603d3f9c6e2c636a58fd125dabe4f5426
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-02-12 21:49:21 +0530

    add Speciality Menu Section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecalityMenu from '../components/SpecalityMenu'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <SpecalityMenu/>
     </div>
   )
 }
