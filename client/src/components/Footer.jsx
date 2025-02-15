a4e8f55798cfdda7d2ce2f80fd11c28cf0a5ea55
rahul reddy <dhanapanarahulreddy4444@gmail.com>
2025-02-16 00:48:00 +0530

    add links to the footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-
+import { useNavigate } from 'react-router-dom'
 const Footer = () => {
+    const navigate = useNavigate();
   return (
     <div className='md:mx-10'>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
             {/* --------left section -------*/}
             <div>
-                <img className='mb-5 w-40' src={assets.logo} alt="" />
+                <img onClick={()=>navigate('/')} className='mb-5 w-40' src={assets.logo} alt="" />
                 <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quam aliquam reprehenderit numquam rerum, magni dolore similique vitae distinctio, commodi possimus? Aspernatur qui fugiat accusantium tenetur ab eos tempora quia?</p>
             </div>
 
@@ -16,10 +17,10 @@ const Footer = () => {
             <div>
                 <p  className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Privacy Policy</li>
+                    <li onClick={()=>navigate('/')} className='cursor-pointer'>Home</li>
+                    <li onClick={()=>navigate('/about')} className='cursor-pointer'>About Us</li>
+                    <li onClick={()=>navigate('/contact')} className='cursor-pointer'>Contact Us</li>
+                    <li><a href='https://github.com/Rahulreddy4444'>Other Products</a></li>
                 </ul>
             </div>
 
@@ -27,8 +28,8 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
                 <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>+91-9938931310</li>
-                    <li>healnow108@gmail.com</li>
+                    <li><a href="tel:+918838823880">+91-8838823880</a></li>
+                    <li><a  href='mailto:healnow108@gmail.com'>healnow108@gmail.com</a></li>
                 </ul>
             </div>
         </div>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <p>ABOUT <span className='text-gray-700 font-medium'>US</span></p>
       </div>
 
-      <div className='my-10 flex felx-col md:flex-row gap-12'>
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
           <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
